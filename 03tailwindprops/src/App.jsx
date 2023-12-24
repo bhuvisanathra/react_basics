7886cfce2d0964bbb567fa2d0e0b263a2afacf5b
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2023-12-24 23:26:09 +0530

    Initial Commit

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Navbar from './Navbar'
+import Navbar2 from './Navbar2'
+
+function App() {
+
+  return (
+    <>
+    <h1>Page-1</h1>
+      <Navbar/>
+    <h1>Page-2</h1>
+    <Navbar2/>
+    </>
+  )
+}
+
+export default App
