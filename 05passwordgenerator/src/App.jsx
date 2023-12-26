80eb53747991cbdf90411542b26dcb44b99c0440
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2023-12-26 22:57:21 +0530

    Updated Password Generator and Screenshot Added!

--- 05passwordgenerator/src/App.jsx
+++ 05passwordgenerator/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
   return (
     <>
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
-      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <h1 className='text-white text-center my-3'>Bhuvi's Password Generator</h1>
       <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input 
           type="text" 
